34d51dc6083bb3cb3ce1ff4a9acda0d10c60318f
Konstantin Denerz <konstantin.denerz@gmail.com>
2021-11-25 08:39:38 +0100

    ng add @ngrx/store

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        MatToolbarModule
+        MatToolbarModule,
+        StoreModule.forRoot({}, {})
     ],
   providers: [],
   bootstrap: [AppComponent]
