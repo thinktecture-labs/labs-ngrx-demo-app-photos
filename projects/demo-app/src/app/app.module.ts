9141cb9fc009207653b16db586332bdf13b7c5a4
Konstantin Denerz <konstantin.denerz@gmail.com>
2021-11-25 08:40:22 +0100

    ng add @ngrx/effects

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { StoreModule } from '@ngrx/store';
         AppRoutingModule,
         BrowserAnimationsModule,
         MatToolbarModule,
-        StoreModule.forRoot({}, {})
+        StoreModule.forRoot({}, {}),
+        EffectsModule.forRoot([])
     ],
   providers: [],
   bootstrap: [AppComponent]
